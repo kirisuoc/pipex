f043f357e8688490596f58ffaf74f60c14a6cc88
kirisuoc <erikcousillas@icloud.com>
2024-11-03 12:35:18 +0100

    Program finished (with comments)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 11:04:23 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/11/03 11:06:24 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/11/03 12:34:16 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,68 +15,17 @@
 void	free_args(char **args);
 void	process_command(char *command_full, char **envp);
 
-int	main(int argc, char **argv, char **envp)
+void	execute_command(int in_fd, int out_fd, char *command, char	**envp)
 {
-	if (argc < 4)
-	{
-		ft_printf("Se esperaban los siguientes argumentos: infile \"comando1\" \"comando2\" outfile\n");
-		return (1);
-	}
+		char	**args;
+		char	*command_path;
 
-	int	fd_infile;
-	int	fd_outfile;
-	int	pipe_fd[2];
-	pid_t pid1;
-	pid_t pid2;
-	char	**args;
-	char	*command_path;
+	 	dup2(in_fd, STDIN_FILENO); // Redirigir stdin al archivo de entrada
+ 		dup2(out_fd, STDOUT_FILENO); // Redirigir stdout al pipe
+		close(in_fd); // Cerrar el extremo de escritura después de redirigir
+		close(out_fd); // Cerrar el archivo de entrada
 
-	// Abre el archivo de entrada
-	fd_infile = open(argv[1], O_RDONLY);
-	if (fd_infile < 0)
-	{
-		perror("Error al abrir infile");
-		exit(EXIT_FAILURE);
-	}
-
-	// Crea el pipe
-	if (pipe(pipe_fd) == -1)
-	{
-		perror("Error al crear el pipe");
-		close(fd_infile); // Se cierra el archivo si hay un error
-		exit(EXIT_FAILURE);
-	}
-
-
-	// Abre el archivo de salida
-	fd_outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_outfile < 0)
-	{
-		perror("Error al abrir outfile");
-		close(fd_infile);
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-		exit(1);
-	}
-
-	// Primer proceso
-	pid1 = fork();
-	if (pid1 == -1)
-	{
-		perror("Error al crear el fork1");
-		exit(EXIT_FAILURE);
-	}
-
-	if (pid1 == 0) // Proceso hijo
-	{
-		close(pipe_fd[0]); // Cerrar el extremo de lectura
- 		dup2(fd_infile, STDIN_FILENO); // Redirigir stdin al archivo de entrada
- 		dup2(pipe_fd[1], STDOUT_FILENO); // Redirigir stdout al pipe
-		close(pipe_fd[1]); // Cerrar el extremo de escritura después de redirigir
-		close(fd_infile); // Cerrar el archivo de entrada
-
-		args = get_command_args(argv[2]);
-
+		args = get_command_args(command);
 		command_path = get_command_path(args[0], envp);
 		if (!command_path)
 		{
@@ -85,54 +34,102 @@ int	main(int argc, char **argv, char **envp)
 		}
 
 		execve(command_path, args, NULL); // Ejecutar el comando con sus argumentos (Ej: ls -l)
-		perror("Error al ejecutar el comando1");
+		perror("Error al ejecutar el comando");
 		free_args(args);
 		free(command_path);
 		exit(EXIT_FAILURE);
-	}
-	else // Proceso padre
+}
+
+int		open_file(char *filename, int flags, mode_t mode, char *error_message)
+{
+	int		fd;
+
+	fd = open(filename, flags, mode);
+	if (fd < 0)
 	{
-		close(pipe_fd[1]); // Cerrar el extremo de escritura
-		waitpid(pid1, NULL, 0); // Esperar a que el primer hijo termine
-
-		// Segundo proceso
-		pid2 = fork();
-		if (pid2 == -1)
-		{
-			perror("Error al crear el fork2");
-			exit(EXIT_FAILURE);
-		}
-		if (pid2 == 0) // Proceso hijo para el segundo comando
-		{
-			dup2(pipe_fd[0], STDIN_FILENO); // Redirigir stdin al pipe
-			dup2(fd_outfile, STDOUT_FILENO); // Redirigir stdout al archivo de salida
-			close(pipe_fd[0]); // Cerrar el extremo de lectura
-			close(fd_outfile); // Cerrar el fd del archivo de salida
-
-			args = get_command_args(argv[3]);
-			command_path = get_command_path(args[0], envp);
-			if (!command_path)
-			{
-				free_args(args);
-				exit(EXIT_FAILURE);
-			}
-
-			execve(command_path, args, NULL); // Ejecutar el comando con sus argumentos (Ej: ls -l)
-			perror("Error al ejecutar el comando2");
-			free_args(args);
-			free(command_path);
-			exit(EXIT_FAILURE);
-		}
-		else // Proceso padre
-		{
-			close(pipe_fd[0]); // Cerrar extremo de lectura del pipe
-			close(fd_outfile); // Cerrar fd del archivo de salida
-			waitpid(pid2, NULL, 0); // Esperar a que el segundo hijo termine
-		}
+		perror(error_message);
+		exit(EXIT_FAILURE);
 	}
+	return (fd);
+}
 
+void	cleanup(int fd_infile, int fd_outfile, int pipe_fd[2])
+{
+	if (fd_infile != 1)
+		close(fd_infile);
+	if (fd_outfile != 1)
+		close(fd_outfile);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+}
 
-	close(fd_infile); // Se cierra el archivo al final
+void	initialize_files(int *fd_infile, int *fd_outfile, char **argv)
+{
+	*fd_infile = open_file(argv[1], O_RDONLY, 0, "Error al abrir infile");
+	*fd_outfile = open_file(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644, "Error al abrir outfile");
+
+}
+
+void	create_pipe(int pipe_fd[2])
+{
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("Error al crear el pipe");
+		exit(EXIT_FAILURE);
+	}
+}
+
+pid_t		safe_fork(void)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error al crear el fork");
+		exit(EXIT_FAILURE);
+	}
+	return (pid);
+}
+
+void	run_pipeline(char **argv, char **envp)
+{
+	int	fd_infile;
+	int	fd_outfile;
+	int	pipe_fd[2];
+	pid_t pid1;
+	pid_t pid2;
+
+	initialize_files(&fd_infile, &fd_outfile, argv);
+	create_pipe(pipe_fd);
+	pid1 = safe_fork();
+	if (pid1 == 0) // Proceso hijo para el primer comando
+	{
+		close(pipe_fd[0]); // Cerrar el extremo de lectura
+		execute_command(fd_infile, pipe_fd[1], argv[2], envp);
+		exit(EXIT_SUCCESS); // Forzar la terminación del proceso hijo
+	}
+	close(pipe_fd[1]); // Cerrar el extremo de escritura
+	waitpid(pid1, NULL, 0); // Esperar a que el primer hijo termine
+	pid2 = safe_fork();
+	if (pid2 == 0) // Proceso hijo para el segundo comando
+	{
+		execute_command(pipe_fd[0], fd_outfile, argv[3], envp);
+		exit(EXIT_SUCCESS); // Forzar la terminación del proceso hijo
+	}
+	cleanup(fd_infile, fd_outfile, pipe_fd);
+	waitpid(pid2, NULL, 0); // Esperar a que el segundo hijo termine
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc < 5)
+	{
+		ft_printf("Se esperaban los siguientes argumentos: "
+		"infile \"comando1\" \"comando2\" outfile\n");
+		return (1);
+	}
+	run_pipeline(argv, envp);
 	return (0);
 }
 
