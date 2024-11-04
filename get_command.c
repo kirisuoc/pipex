a4aa8b4a8756bc914cc82802db05155c0d204fff
Erik Cousillas Fernandez <ecousill@c3r4s6.42urduliz.com>
2024-11-04 12:31:03 +0100

    Trying to fix quotes

--- get_command.c
+++ get_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_command.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
+/*   By: ecousill <ecousill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 02:23:33 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/11/03 23:17:09 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/11/04 12:29:19 by ecousill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,53 @@ char	*remove_quotes(char *str)
 char	**get_command_args(char *command_full)
 {
 	char	**args;
+	char	*arg;
+	int		in_quotes;
 	int		i;
-	char	*new_arg;
+	int		start;
+	int		arg_count;
+	int		length;
 
-	args = ft_split(command_full, ' ');
-
+	args = malloc((ft_strlen(command_full) + 2) * sizeof(char *));
+	if (!args)
+		return (NULL);
+	in_quotes = 0;
 	i = 0;
-	while (args[i] != NULL)
+	start = 0;
+	arg_count = 0;
+
+	while (command_full[i] != '\0')
 	{
-		new_arg = remove_quotes(args[i]);
-		free(args[i]);
-		args[i] = new_arg;
+		if ((command_full[i] == '\'' && (i == 0 || command_full[i - 1] != '\\'))
+			|| (command_full[i] == '\"' && (i == 0 || command_full[i - 1] != '\\')))
+			in_quotes = !in_quotes;
+
+		if ((command_full[i] == ' ' && !in_quotes) || command_full[i + 1] == '\0')
+		{
+			if (command_full[i + 1] == '\0')
+			{
+				length = i - start + 1;
+			}
+			else
+			{
+				length = i - start;
+			}
+			arg = malloc(length + 1);
+			if (!arg)
+				return (NULL);
+			ft_strlcpy(arg, &command_full[start], length + 1);
+			args[arg_count++] = remove_quotes(arg);
+			free(arg);
+
+			start = i + 1;
+		}
 		i++;
 	}
+	printf("%s\n", args[0]);
+	printf("%s\n", args[1]);
+	printf("%s\n", args[2]);
+
+	args[arg_count] = NULL;
 	return (args);
 }
 
